a7ee06799dfcf0d16a63a6db0461e4629e85a796
Marcos Tullio Silva de Souza <marcostullio.s@gmail.com>
2025-03-03 01:37:55 -0300

    segundo commit

---
+++ src/RoutesComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Repositorio from './pages/Repositorio';
+
+export default function RoutesComponent() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/repositorio" element={<Repositorio />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
